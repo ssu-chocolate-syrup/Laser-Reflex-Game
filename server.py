9d3060802a8f776f621135362acda570caa31cfe
Ted Song <kitae040522@gmail.com>
2023-11-29 16:25:08 +0900

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,76 @@ from _thread import *
 
 client_sockets = []
 
-HOST = socket.gethostbyname(socket.gethostname())
+## Server IP and Port ##
+
+HOST = '0.0.0.0'
+PORT = 9999
+
+########## processing in thread ##
+## new client, new thread ##
+
+def threaded(client_socket, addr):
+  print('>> Connected by :', addr[0], ':', addr[1])
+
+  ## process until client disconnect ##
+  while True:
+    try:
+      ## send client if data recieved(echo) ##
+      data = client_socket.recv(1024)
+
+      if not data:
+        print('>> Disconnected by ' + addr[0], ':', addr[1])
+        break
+
+      print('>> Received from ' + addr[0], ':', addr[1], data.decode())
+
+      ## chat to client connecting client ##
+      ## chat to client connecting client except person sending message ##
+      for client in client_sockets:
+        if client != client_socket:
+          client.send(data)
+     
+    except ConnectionResetError as e:
+      print('>> Disconnected by ' + addr[0], ':', addr[1])
+      break
+   
+  if client_socket in client_sockets:
+    client_sockets.remove(client_socket)
+    print('remove client list : ', len(client_sockets))
+
+  client_socket.close()
+
+############# Create Socket and Bind ##
+
+print('>> Server Start with ip :', HOST)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((HOST, PORT))
+server_socket.listen()
+
+############# Client Socket Accept ##
+
+try:
+  while True:
+    print('>> Wait')
+
+    client_socket, addr = server_socket.accept()
+    client_sockets.append(client_socket)
+    start_new_thread(threaded, (client_socket, addr))
+    print("참가자 수 : ", len(client_sockets))
+except Exception as e:
+  print('에러 : ', e)
+
+finally:
+  server_socket.close()
+
+'''
+import socket
+from _thread import *
+
+client_sockets = []
+
+HOST = socket.gethostbyname('')
 PORT = 9999
 
 def threaded(client_socket, addr):
@@ -51,4 +120,4 @@ except Exception as e:
     print('에러 : ', e)
 
 finally:
-    server_socket.close()
+    server_socket.close()'''
