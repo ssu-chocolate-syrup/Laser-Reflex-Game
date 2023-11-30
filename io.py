b3934e4aab1b9f2154e11de41af2ef1cb05d05c4
kitae0522 <kitae040522@gmail.com>
2023-11-30 20:37:09 +0900

    feat: update io.py

--- io.py
+++ io.py
@@ -1,30 +1,40 @@
-#turn on LED
-def set_led(num_pico, num_button, rgb_code):
-    if num_pico == deviceID and 0 <= num_button < 16:
-        keypad.illuminate(num_button, *rgb_code)
-        keypad.update()
+from typing import Tuple
+import picokeypad
 
-#pico Pi signal to Matrix signal
-def input_interface(num_pico, num_button):
-    k = num_pico // 2
+keypad = picokeypad.PicoKeyPad()
+keypad.set_brightness(1.0)
 
-    if num_pico % 2 == 1:
-        row = (3 - (num_button % 4)) + (k * 4)
-        col = num_button // 4
-    else:
-        row = (num_button % 4) + (k * 4)
-        col = 3 - (num_button // 4)
+class IO:
+    #turn on LED
+    def set_led(self, num_pico: int, num_button: int, rgb_code: Tuple[int, int, int]) -> None:
+        if num_pico == deviceID and 0 <= num_button < 16:
+            keypad.illuminate(num_button, *rgb_code)
+            keypad.update()
 
-    return row, col
+    #pico Pi signal to Matrix signal
+    def input_interface(self, num_pico: int, num_button: int) -> Tuple[int, int]:
+        k = num_pico // 2
 
-#Matrix signal to pico Pi signal
-def output_interface(row, col, rgb_code):
-    if col < 4:
-        num_pico = 2 * (row % 4 + 1) - 1
-        num_button = 4 * (col % 4) + (3 - (row % 4))
-    else:
-        num_pico = 2 * (row % 4 + 1)
-        num_button = 4 * (3 - (col % 4)) + (row % 4)
-    
-    #call set_led
-    set_led(num_pico, num_button, rgb_code)
+        if num_pico % 2 == 1:
+            row = (3 - (num_button % 4)) + (k * 4)
+            col = num_button // 4
+        else:
+            row = (num_button % 4) + (k * 4)
+            col = 3 - (num_button // 4)
+
+        return tuple(row, col)
+
+    #Matrix signal to pico Pi signal
+    def output_interface(self, row: int, col: int) -> Tuple[int, int]:
+        if col < 4:
+            num_pico = 2 * (row % 4 + 1) - 1
+            num_button = 4 * (col // 4) + (3 - (row % 4))
+        else:
+            num_pico = 2 * (row % 4 + 1)
+            num_button = 4 * (3 - (col // 4)) + (row % 4)
+        
+        return tuple(num_pico, num_button)
+
+    def turn_on(self, row: int, col: int, rgb_code: Tuple[int, int, int]) -> None:
+        num_pico, num_button = output_interface(row, col)
+        set_led(num_pico, num_button, rgb_code)
