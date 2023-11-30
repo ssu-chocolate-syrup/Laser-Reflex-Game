910b5baca525b48d6cf7139ac05a4c9f46accc76
kitae0522 <kitae040522@gmail.com>
2023-11-30 18:05:40 +0900

    feat: update server

--- server.py
+++ server.py
@@ -1,13 +1,9 @@
 import socket
 from _thread import *
 
+from server_config import Server
+
 client_sockets = []
-
-## Server IP and Port ##
-
-HOST = '0.0.0.0'
-PORT = 9999
-
 
 ########## processing in thread ##
 ## new client, new thread ##
@@ -45,10 +41,10 @@ def threaded(client_socket, addr):
 
 ############# Create Socket and Bind ##
 
-print('>> Server Start with ip :', HOST)
+print('>> Server Start with ip :', Server.HOST)
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((HOST, PORT))
+server_socket.bind((Server.HOST, Server.PORT))
 server_socket.listen()
 
 ############# Client Socket Accept ##
