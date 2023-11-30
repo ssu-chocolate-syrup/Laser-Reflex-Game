8e1ade2b888547fafba3f3c57a6d4442051d9b92
yumin0544 <136509835+yumin0544@users.noreply.github.com>
2023-11-30 20:03:46 +0900

    Create io.py

---
+++ io.py
@@ -0,0 +1,30 @@
+#turn on LED
+def set_led(num_pico, num_button, red, green, blue):
+    if num_pico == deviceID and 0 <= num_button < 16:
+        keypad.illuminate(num_button, red, green, blue)
+        keypad.update()
+
+#pico Pi signal to Matrix signal
+def input_interface(num_pico, num_button):
+    k = num_pico // 2
+
+    if num_pico % 2 == 1:
+        row = (3 - (num_button % 4)) + (k * 4)
+        col = num_button // 4
+    else:
+        row = (num_button % 4) + (k * 4)
+        col = 3 - (num_button // 4)
+
+    return row, col
+
+#Matrix signal to pico Pi signal
+def output_interface(row, col, R,G,B):
+    if col < 4:
+        num_pico = 2 * (row % 4 + 1) - 1
+        num_button = 4 * (col % 4) + (3 - (row % 4))
+    else:
+        num_pico = 2 * (row % 4 + 1)
+        num_button = 4 * (3 - (col % 4)) + (row % 4)
+    
+    #call set_led
+    set_led(num_pico, num_button, R, G, B)
