b46758dbf68263020e79ab2492f37f4ecb0a7764
kwh0420 <dngus0620@gmail.com>
2023-12-03 12:29:28 +0900

    update,feat :server.py/divide func, add effect func

--- server.py
+++ server.py
@@ -70,6 +70,53 @@ class LaserGameServer:
     #         elif count == 0:
     #             self.timer_thread_function(count)
     #             threading.Thread(target=self.timer_thread, args=(0, count + 4)).start()
+    
+    #반복되는 전송 처리해주기위해 함수 처리
+    def send_to_pico(clinet_socket,send_data):
+        print(send_data)
+        ## 실제 전송
+        client_socket.sendall(struct.pack('!I', len(send_data)))
+        client_socket.sendall(send_data)
+        for client in self.client_sockets:
+            if client != client_socket:
+                client.sendall(struct.pack('!I', len(send_data)))
+                client.sendall(send_data)
+    
+    #승리 이펙트 계산함수 (effect_type이 모든 피코파이의 button_num=0에 도달하면, 각 client에서 이펙트 실행)
+    def win_effect(pico,player):
+        send_data = []
+        if (player == 1):
+            effect_type ='' ##effect type 추가해주세요, player1
+        elif (player == 2):
+            effect_type ='' ##effect type 추가해주세요, player2
+        for i in range(6):
+            send_data.append(dict(
+                c= effect_type, 
+                d= i,
+                b= 0
+            ))
+        return send_data
+    #승리 이펙트 전송함수
+    def effect_to_clients(client_socket):
+        check=self.game_instance.goal_check()
+        if (check==1): ##p1이 승리
+            send_data = json.dumps(win_effect(i,1))
+            send_to_pico(client_socket,send_data)
+            time(1)
+            #이펙트 출력 완료까지 대기
+            time.sleep(1)
+
+        elif (check==2): ##p2가 승리
+            send_data = json.dumps(win_effect(i,2))
+            send_to_pico(client_socket,send_data)
+            #이펙트 출력 완료까지 대기
+            time.sleep(1)
+
+    #dfs 전송 함수
+    def dfs_to_clients(client_socket,row,col):
+        self.game_instance.input_mirror(row, col)
+        send_data = json.dumps(self.game_instance.main()).encode()
+        send_to_pico(client_socket,send_data)
 
     def threaded(self, client_socket, addr):
         print('>> Connected by :', addr[0], ':', addr[1])
@@ -79,20 +126,17 @@ class LaserGameServer:
                 data = self.recv_data(client_socket)
                 print('>> Received from ' + addr[0], ':', addr[1], data)
                 row, col = self.pico_interface.input_interface(data['d'], data['b'])
-                if row == 0 and col == 0:
+                #턴버튼 7,5 or 7,6으로 추가 effect_send함수 추가
+                if (row == 7 and col == 5)or(row == 7 and col == 6):
                     # threading.Thread(target=self.timer_thread, args=(0, 4)).start()
-                    send_data = json.dumps(self.game_instance.main()).encode()
-                else:
-                    self.game_instance.input_mirror(row, col)
-                    send_data = json.dumps(self.game_instance.main()).encode()
-                print(send_data)
-                client_socket.sendall(struct.pack('!I', len(send_data)))
-                client_socket.sendall(send_data)
-                for client in self.client_sockets:
-                    if client != client_socket:
-                        client.sendall(struct.pack('!I', len(send_data)))
-                        client.sendall(send_data)
+                    #이펙트 전송 함수
+                    effect_to_clients(client_socket)
+                    #dfs 전송 함수
+                    dfs_to_clients(client_socket,row,col)
 
+                else:
+                    #dfs 전송함수
+                    dfs_to_clients(client_socket,row,col)
             except ConnectionResetError as e:
                 print('>> Disconnected by ' + addr[0], ':', addr[1])
                 break
