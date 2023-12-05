a496a8a8566072d853d4cfc5df96b3b7fb97588e
yumin0544 <136509835+yumin0544@users.noreply.github.com>
2023-12-06 01:14:29 +0900

    Create sound.py

---
+++ server/util/sound.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Sound:
+    def clear(self):
+        pygame.mixer.init()
+
+    def bgm(self):
+        pygame.mixer.stop()
+        sound_effect = pygame.mixer.Sound('bgm.mp3')
+        sound_effect.play()
+
+    def turn_change(self):
+        pygame.mixer.stop()
+        sound_effect = pygame.mixer.Sound('turn.mp3')
+        sound_effect.play()
+
+    def game_over(self):
+        pygame.mixer.stop()
+        sound_effect = pygame.mixer.Sound('gameover.mp3')
+        sound_effect.play()
