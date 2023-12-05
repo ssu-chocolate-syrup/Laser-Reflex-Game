ec39ed00f90141da4e9db3f692ec7749c04bbf09
Yang Yewon <imdnjs423@gmail.com>
2023-12-06 01:47:14 +0900

    feat : only try for p1segment

--- 7segment.py
+++ 7segment.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 digitBitmap = { 0: 0b00111111, 1: 0b00000110, 2: 0b01011011, 3: 0b01001111, 4: 0b01100110, 5: 0b01101101, 6: 0b01111101, 7: 0b00000111, 8: 0b01111111, 9: 0b01100111 }
+masks = { 'a': 0b00000001, 'b': 0b00000010, 'c': 0b00000100, 'd': 0b00001000, 'e': 0b00010000, 'f': 0b00100000, 'g': 0b01000000 }
 p1_pins = {'a':17,'b':27,'c':22,'d':10,'e': 9,'f': 11,'g':0}
 
 def p1_segment(c):
@@ -15,16 +16,15 @@ def p1_segment(c):
             
 GPIO.setwarnings(False)
 
-
 try:
     GPIO.setup(list(p1_pins.values()),GPIO.OUT)
     GPIO.output(list(p1_pins.values()),GPIO.LOW)
   
-    p2_segment(5)
+    p1_segment(5)
 
 except KeyboardInterrupt:
     print("fail:(")
 
 finally:
-    time.sleep(5)
+    time.sleep(10)
     GPIO.cleanup()
