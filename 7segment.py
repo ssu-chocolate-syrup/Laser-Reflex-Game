b4e0145ceba2f4c9aa84bbf1158309b6cd588a81
Yang Yewon <imdnjs423@gmail.com>
2023-12-06 01:40:12 +0900

    feat: only try for p1_segment

--- 7segment.py
+++ 7segment.py
@@ -4,30 +4,27 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 digitBitmap = { 0: 0b00111111, 1: 0b00000110, 2: 0b01011011, 3: 0b01001111, 4: 0b01100110, 5: 0b01101101, 6: 0b01111101, 7: 0b00000111, 8: 0b01111111, 9: 0b01100111 }
-masks = { 'a': 0b00000001, 'b': 0b00000010, 'c': 0b00000100, 'd': 0b00001000, 'e': 0b00010000, 'f': 0b00100000, 'g': 0b01000000 }
-pins = { 'a': 17, 'b': 27, 'c': 22, 'd': 10, 'e': 9, 'f': 11, 'g': 0}
+p1_pins = {'a':17,'b':27,'c':22,'d':10,'e': 9,'f': 11,'g':0}
 
-def renderChar(c):
+def p1_segment(c):
     val = digitBitmap[c]
-
-    GPIO.output(list(pins.values()), GPIO.LOW)
-
+    GPIO.output(list(p1_pins.values()), GPIO.HIGH)
     for k,v in masks.items():
-        if val&v == v:
-            GPIO.output(pins[k], GPIO.HIGH)
+        if val&v==v:
+            GPIO.output(p1_pins[k], GPIO.LOW)
+            
+GPIO.setwarnings(False)
+
 
 try:
-    GPIO.setup(list(pins.values()), GPIO.OUT)
-    GPIO.output(list(pins.values()), GPIO.LOW)
+    GPIO.setup(list(p1_pins.values()),GPIO.OUT)
+    GPIO.output(list(p1_pins.values()),GPIO.LOW)
+  
+    p2_segment(5)
 
-    val = 0
-
-    while True:
-        renderChar(val)
-        val = 0 if val == 9 else (val + 1)
-        time.sleep(1)
-        
 except KeyboardInterrupt:
-    print("Goodbye")
+    print("fail:(")
+
 finally:
+    time.sleep(5)
     GPIO.cleanup()
