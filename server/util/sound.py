d92d69f76c35bcd2770fcb4b284890410ae65386
yumin0544 <136509835+yumin0544@users.noreply.github.com>
2023-12-06 21:20:57 +0900

    Update sound.py

--- server/util/sound.py
+++ server/util/sound.py
@@ -1,22 +1,24 @@
 '''
-import pygame
+from audioplayer import AudioPlayer
 
 class Sound:
     def __init__(self):
-        pygame.mixer.init()
-        self.bgm_sound = pygame.mixer.Sound('bgm.mp3')
-        self.turn_sound = pygame.mixer.Sound('turn.mp3')
-        self.game_over_sound = pygame.mixer.Sound('gameover.mp3')
+        self.bgm_sound = AudioPlayer("bgm.mp3")
+        self.turn_sound = AudioPlayer("turn.mp3")
+        self.game_over_sound = AudioPlayer("gameover.mp3")
 
     def play_bgm(self):
-        pygame.mixer.stop()
-        self.bgm_sound.play()
+        self.bgm_sound.play(loop=True)
 
     def play_turn_change(self):
-        pygame.mixer.stop()
         self.turn_sound.play()
 
     def play_game_over(self):
-        pygame.mixer.stop()
+        # 다른 모든 사운드 정지
+        self.bgm_sound.stop()
+        self.turn_sound.stop()
+
+        # 게임 오버 사운드 재생
         self.game_over_sound.play()
+
 '''
