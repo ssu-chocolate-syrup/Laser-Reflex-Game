bc6532e20ef63dc93659a9e605d27d46baa1d2ba
kwh0420 <dngus0620@gmail.com>
2023-12-07 12:44:21 +0900

    fix: 7seg init

--- server/util/segment.py
+++ server/util/segment.py
@@ -2,11 +2,11 @@ import time
 import RPi.GPIO as GPIO
 
 class SevenSegmentDisplay:
-    def __init__(self, p1_pins, p2_pins, digitBitmap, masks):
-        self.p1_pins = p1_pins
-        self.p2_pins = p2_pins
-        self.digitBitmap = digitBitmap
-        self.masks = masks
+    def __init__(self):
+        self.p1_pins = {'a': 17, 'b': 27, 'c': 22, 'd': 10, 'e': 9, 'f': 11, 'g': 0}
+        self.p2_pins = {'a': 23, 'b': 24, 'c': 25, 'd': 8, 'e': 7, 'f': 1, 'g': 12}
+        self.digitBitmap = {1: 0b00000110, 2: 0b01011011, 3: 0b01001111, 5: 0b01101101, 6: 0b01111101, 7: 0b00000111}
+        self.masks = masks = {'a': 0b00000001, 'b': 0b00000010, 'c': 0b00000100, 'd': 0b00001000, 'e': 0b00010000, 'f': 0b00100000, 'g': 0b01000000}
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
@@ -40,9 +40,7 @@ class SevenSegmentDisplay:
 
 # Example usage
 if __name__ == "__main__":
-    digitBitmap = {1: 0b00000110, 2: 0b01011011, 3: 0b01001111, 5: 0b01101101, 6: 0b01111101, 7: 0b00000111}
     masks = {'a': 0b00000001, 'b': 0b00000010, 'c': 0b00000100, 'd': 0b00001000, 'e': 0b00010000, 'f': 0b00100000, 'g': 0b01000000}
-    p1_pins = {'a': 17, 'b': 27, 'c': 22, 'd': 10, 'e': 9, 'f': 11, 'g': 0}
-    p2_pins = {'a': 23, 'b': 24, 'c': 25, 'd': 8, 'e': 7, 'f': 1, 'g': 12}
+    
     
     display = SevenSegmentDisplay(p1_pins, p2_pins, digitBitmap, masks)
